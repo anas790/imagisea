ce66e41fdaae0d338ffe1ff12de8ee4826678617
harshs0203 <harshs0203@gmail.com>
2023-04-16 19:35:45 +0530

    Finalize Screen updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:imagisea/screens/loading_screen.dart';
 import 'package:imagisea/Screens/HomeScreen.dart';
+import 'package:imagisea/Screens/finalized_screen.dart';
 import 'package:imagisea/Screens/generate_screen.dart';
 import 'package:imagisea/Screens/profile_screen.dart';
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: const Loading_screen(),
-      home: const Generate_screen(),
+      home: const Finalized_screen(),
     );
   }
 }
