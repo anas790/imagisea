9ac05e655fc6d8621cd46496a89c2ff7694b2af8
nistha <nisthasharma0799@gmail.com>
2023-10-02 23:11:25 +0530

    firebase connection

---
+++ lib/Screens/nft_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class nft_screen extends StatefulWidget {
+  const nft_screen({Key? key}) : super(key: key);
+
+  @override
+  State<nft_screen> createState() => _nft_screenState();
+}
+
+class _nft_screenState extends State<nft_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        
+
+      ),
+
+
+    );
+  }
+}
+
